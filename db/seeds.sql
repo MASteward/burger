08bd67da320e89a2472ce09a86443f55b0037a67
MASteward <msteward05@hotmail.com>
2018-03-04 16:42:19 -0600

    reset folders without sequelize

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Burger", false);
-INSERT INTO burgers (burger_name, devoured) VALUES ("Cheeseburger", false);
-INSERT INTO burgers (burger_name, devoured) VALUES ("Hamburger", false);
+INSERT INTO burgers (burger_name) VALUES ("Bacon Burger");
+INSERT INTO burgers (burger_name) VALUES ("Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Hamburger");
